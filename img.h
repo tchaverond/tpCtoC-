f62e97c0faedbd361109745b5e0c3bcd38ea8588
Chaverondier Thomas <tchaverond@bs406-s16-04.insa-lyon.fr>
2014-11-17 15:48:51 +0100

    added required (compiling) functions to img.cpp
    write_to_file, read_from_file, desaturate, shrink

--- img.h
+++ img.h
@@ -10,6 +10,7 @@ class img
  public :
   img(void);
   img(const img&);
+
   void ppm_write_to_file(int width, int height, unsigned char* data, FILE* file, char string[]);
   void ppm_read_from_file(int *width, int *height, unsigned char** data, FILE* file);
   void ppm_desaturate(unsigned char* image, int width, int height);
